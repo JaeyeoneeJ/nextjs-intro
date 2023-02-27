d1398ba2052c168d7d946b67445ee16546cd3760
jaeyeoneej <5ikve@naver.com>
2023-02-27 15:32:13 +0900

    prac: #0 useRouter를 사용하여 스타일 적용 23-02-27
    - react-router-dom을 import하지 않아도 next에 자체적으로 useRouter 훅을 제공함
    - 해당 pathname을 식별해 스타일 적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
+  console.log(router);
   return (
     <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
+      <Link
+        href="/"
+        style={{ color: router.pathname === "/" ? "red" : "blue" }}
+      >
+        Home
+      </Link>
+      <Link
+        href="/about"
+        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+      >
+        About
+      </Link>
     </nav>
   );
 }
