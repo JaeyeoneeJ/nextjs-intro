a81079c97b711db9ee38fa0a047552ce5216d39e
jaeyeoneej <5ikve@naver.com>
2023-02-27 19:24:51 +0900

    feat: #0 api 키를 숨기기 위해 rewrites를 적용 23-02-27
    - api 키가 request 등으로 노출되는 것을 막기 위해 가짜 url을 생성하여 보여줌

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-const API_KEY = "b298f2c7fb554844e7980782dca6813f";
-
 export default function Home() {
   const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
