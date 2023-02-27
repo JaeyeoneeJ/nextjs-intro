29036cd9088fe66198fae4c1a9182fbeb2590969
jaeyeoneej <5ikve@naver.com>
2023-02-27 14:41:59 +0900

    test: #0 자바스크립트 연결해보기 23-02-27
    - nextjs 안에서 reactjs를 불러올 수 있음
    - 서버단에서 로딩되기 때문에 html 소스가 그대로 보임
    => 기존 react는 div id=root 안에 빈 것으로밖에 보이지 않음
    - 이를 통해 SEO에서 큰 장점을 가질 수 있음

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import { useState } from "react";
+
 export default function Home() {
-  return "hi";
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
