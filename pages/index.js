300a02f65b1ae16e293a1a8efc3bb1889db43db0
jaeyeoneej <5ikve@naver.com>
2023-02-27 17:36:05 +0900

    feat: #0 movie api를 fetching 함 23-02-27

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "b298f2c7fb554844e7980782dca6813f";
+
 export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
