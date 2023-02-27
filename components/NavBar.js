7c4816e86d5bcc12e083c24d7572dab630487426
jaeyeoneej <5ikve@naver.com>
2023-02-27 15:58:28 +0900

    prac: #0 styled jsx로 한정된 scope에 style 적용 23-02-27

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,28 +1,24 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
     <nav>
-      <Link
-        href="/"
-        className={`${styles.link}
-        ${router.pathname === "/" ? styles.active : ""}`}
-      >
-        Home
+      <Link href="/" legacyBehavior>
+        <a className={router.pathname === "/" ? "active" : ""}>Home</a>
       </Link>
-      <Link
-        href="/about"
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : "",
-        ].join(" ")}
-      >
-        About
+      <Link href="/about" legacyBehavior>
+        <a className={router.pathname === "/about" ? "active" : ""}>About</a>
       </Link>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: tomato;
+        }
+      `}</style>
     </nav>
   );
 }
