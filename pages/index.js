3fc6830d047faf5dd8857bc4ca4a609572976a0f
jaeyeoneej <5ikve@naver.com>
2023-02-27 21:17:11 +0900

    add: #0 각 movie를 클릭 시 detail 페이지로 넘어갈 수 있도록 함 23-02-27

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id) => {
+    router.push(`/movies/${id}`);
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div className="movie" key={movie.id} onClick={() => onClick(movie.id)}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <Link href={`/movies/${movie.id}`} legacyBehavior>
+            <a>
+              <h4>{movie.original_title}</h4>
+            </a>
+          </Link>
         </div>
       ))}
       <style jsx>{`
@@ -17,6 +27,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
