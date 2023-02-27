8e6715a75aa6f7d503e2e89048f9a8af87f971f9
jaeyeoneej <5ikve@naver.com>
2023-02-27 15:42:28 +0900

    prac: #0 css module을 사용하여 패턴화 23-02-27
    - css module을 사용하여 패턴화함.
    - 동시에 여러개의 클래스를 주려고 할 때, 코드가 복잡함

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
@@ -8,13 +9,17 @@ export default function NavBar() {
     <nav>
       <Link
         href="/"
-        style={{ color: router.pathname === "/" ? "red" : "blue" }}
+        className={`${styles.link}
+        ${router.pathname === "/" ? styles.active : ""}`}
       >
         Home
       </Link>
       <Link
         href="/about"
-        style={{ color: router.pathname === "/about" ? "red" : "blue" }}
+        className={[
+          styles.link,
+          router.pathname === "/about" ? styles.active : "",
+        ].join(" ")}
       >
         About
       </Link>
